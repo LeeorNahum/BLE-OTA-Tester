8b357fa3d70d208bd39a5c299e2f9203a3fe65b6
LeeorMiner <leeornahum@gmail.com>
2024-10-06 16:32:17 -0400

    major move to FastBLEOTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,144 +1,86 @@
 #include <NimBLEDevice.h>
-#include <Update.h>
+#include <FastBLEOTA.h>
 #include <UMS3.h>
+#include <SimpleDebug.h>
 #include <esp_sleep.h>
+#include "esp_err.h"
+#include "esp_pm.h"
 
-NimBLECharacteristic *pCharacteristic;
-bool newDataAvailable = false;
-size_t expectedSize = 0;  // The expected size of the firmware
-size_t receivedSize = 0;  // The size of data received so far
-bool sizeReceived = false; // Flag to check if the size metadata is received
-
-// Create an instance of the UMS3
 UMS3 ums3;
 
 bool deviceConnected = false;
 
-// Callback class for BLE server events
-class MyServerCallbacks: public NimBLEServerCallbacks {
-    void onConnect(NimBLEServer* pServer) {
-        Serial.println("Client connected");
-        deviceConnected = true;
-        ums3.setPixelColor(UMS3::color(0, 255, 0));  // Turn LED green when connected
-    }
+class ServerCallbacks : public NimBLEServerCallbacks {
+  void onConnect(NimBLEServer* pServer) {
+    Serial.println("Client connected, stopping advertising");
+    NimBLEDevice::stopAdvertising();
+    ums3.setPixelColor(UMS3::color(0, 0, 255));  // Turn LED blue when connected
+  }
 
-    void onDisconnect(NimBLEServer* pServer) {
-        Serial.println("Client disconnected, restarting advertising");
-        deviceConnected = false;
-        sizeReceived = false;
-        receivedSize = 0;
-        Update.abort();  // Abort any ongoing update
-        NimBLEDevice::startAdvertising();  // Start advertising again
-        ums3.setPixelColor(UMS3::color(255, 0, 0));  // Turn LED red when disconnected
-    }
+  void onDisconnect(NimBLEServer* pServer) {
+    Serial.println("Client disconnected, starting advertising");
+    NimBLEDevice::startAdvertising();
+    ums3.setPixelColor(UMS3::color(255, 0, 0));  // Turn LED red when disconnected
+  }
 };
 
-class MyCallbacks : public NimBLECharacteristicCallbacks {
-    void onWrite(NimBLECharacteristic *pCharacteristic) {
-        String value = pCharacteristic->getValue();
+class OTACallbacks : public FastBLEOTACallbacks {
+  void onOTAStart(size_t expectedSize) override {
+    Serial.println("MyFastBLEOTA: OTA Update Started");
+    ums3.setPixelColor(UMS3::color(255, 255, 0));  // Yellow
+  }
 
-        if (!sizeReceived) {
-            if (value.length() != sizeof(uint32_t)) {
-                Serial.println("Received size data of incorrect length");
-                return;
-            }
-            expectedSize = *((uint32_t*)value.c_str());
-            sizeReceived = true;
-            Serial.printf("Expected firmware size: %d bytes\n", expectedSize);
+  void onOTAProgress(size_t receivedSize, size_t expectedSize) override {
+    float progress = (float)receivedSize / (float)expectedSize * 100.0;
+    //Serial.printf("MyFastBLEOTA: OTA Progress: %.2f%%\n", progress);
+    Serial.println(progress, 2);
+    // Optionally update LED brightness or color
+  }
 
-            // Start the update process
-            if (!Update.begin(expectedSize)) {
-                Serial.println("Failed to start update");
-                Update.printError(Serial);
-                return;
-            }
-        } else {
-            // Write directly to flash
-            size_t chunkSize = value.length();
-            if (Update.write((uint8_t*)value.c_str(), chunkSize) != chunkSize) {
-                Serial.println("Failed to write firmware chunk");
-                Update.printError(Serial);
-                return;
-            }
+  void onOTAComplete() override {
+    Serial.println("MyFastBLEOTA: OTA Update Complete");
+    ums3.setPixelColor(UMS3::color(0, 255, 0));  // Green
+    delay(1000);
+    ESP.restart();
+  }
 
-            receivedSize += chunkSize;
-
-            if (receivedSize > expectedSize) {
-                Serial.println("Received more data than expected");
-                Update.end();
-                return;
-            }
-
-            if (receivedSize == expectedSize) {
-                newDataAvailable = true;
-            }
-        }
-    }
+  void onOTAError(fastbleota_error_t errorCode) override {
+    Serial.printf("MyFastBLEOTA: OTA Error: %d\n", errorCode);
+    ums3.setPixelColor(UMS3::color(255, 0, 0));  // Red
+  }
 };
 
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    NimBLEDevice::init("ESP32_BLE_OTA");
+  setCpuFrequencyMhz(80);
+  
+  ums3.begin();
+  ums3.setPixelPower(true);
+  ums3.setPixelBrightness(85);
+  ums3.setPixelColor(UMS3::color(0, 255, 255));
 
-    NimBLEDevice::setMTU(512);  // Set MTU size to maximum supported
+  NimBLEDevice::init("ESP32_BLE_OTA");
+  NimBLEDevice::setMTU(512);
+  NimBLEDevice::setPower(ESP_PWR_LVL_P9);  
+  NimBLEDevice::setSecurityAuth(true, true, true);
+  NimBLEDevice::createServer()->setCallbacks(new ServerCallbacks());
 
-    NimBLEDevice::setPower(ESP_PWR_LVL_P9);  // Max BLE power level
-
-    NimBLEServer *pServer = NimBLEDevice::createServer();
-    pServer->setCallbacks(new MyServerCallbacks());
-
-    // Security setup
-    NimBLEDevice::setSecurityAuth(true, true, true);  // Enable bonding
-
-    NimBLEService *pService = pServer->createService("4e8cbb5e-bc0f-4aab-a6e8-55e662418bef");
-
-    pCharacteristic = pService->createCharacteristic(
-        "513fcda9-f46d-4e41-ac4f-42b768495a85",
-        NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::WRITE_NR
-    );
-
-    pCharacteristic->setCallbacks(new MyCallbacks());
-    pService->start();
-
-    NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
-    pAdvertising->addServiceUUID(pService->getUUID());
-    pAdvertising->setScanResponse(true);
-    pAdvertising->setAppearance(0x02);
-    pAdvertising->start();
-
-    ums3.begin();
-    ums3.setPixelPower(true);
-    ums3.setPixelBrightness(85);  // Set brightness to a third
-    ums3.setPixelColor(UMS3::color(0, 0, 255));  // Initial color
-
-    setCpuFrequencyMhz(240);  // Max CPU frequency
-    esp_sleep_enable_bt_wakeup();
+  FastBLEOTA::setCallbacks(new OTACallbacks());
+  FastBLEOTA::begin(NimBLEDevice::getServer());
+  
+  NimBLEDevice::getAdvertising()->start();
+  esp_pm_config_t pm_config = {
+    .max_freq_mhz = 80,
+    .min_freq_mhz = 40,
+    .light_sleep_enable = false
+  };
 }
 
 void loop() {
-    if (newDataAvailable) {
-        newDataAvailable = false;
-
-        if (receivedSize == expectedSize && expectedSize > 0) {
-            Serial.println("All firmware data received, finalizing update...");
-
-            if (Update.end(true)) {  // True to reboot
-                Serial.println("Update complete. Rebooting...");
-                ums3.setPixelColor(UMS3::color(255, 0, 0));  // Indicate completion
-                delay(1000);
-                ESP.restart();
-            } else {
-                Serial.println("Failed to finalize update");
-                Update.printError(Serial);
-            }
-        }
-    }
-
-    if (!deviceConnected) {
-        int brightness = (int)(sin(millis() / 500.0) * 128 + 128);
-        ums3.setPixelColor(UMS3::color(0, 0, brightness));  // Fade in and out like a sin
-    }
-
-    delay(100);
+  if (!NimBLEDevice::getServer()->getConnectedCount()) {
+    int brightness = (int)(sin(millis() / 500.0) * 128 + 128);
+    ums3.setPixelColor(UMS3::color(0, brightness, 0));  // Fade in and out
+  }
+  delay(100);
 }
